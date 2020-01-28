680b087cf15c0d3a859e99f13e3b8f113da88073
Matt Stacey <53570754+matt-stacey@users.noreply.github.com>
2020-01-28 07:36:03 -0600

    sentdex tensor flow intro

---
+++ Python/sentdex/tensor_flow_load_data.py
@@ -0,0 +1,35 @@
+# https://pythonprogramming.net/loading-custom-data-deep-learning-python-tensorflow-keras/?completed=/introduction-deep-learning-python-tensorflow-keras/
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import cv2
+from tqdm import tqdm
+
+DATADIR = "X:/Datasets/PetImages"
+CATEGORIES = ["Dog", "Cat"]
+IMG_SIZE = 50
+training_data = []
+
+def create_training_data():
+    for category in CATEGORIES:  # do dogs and cats
+
+        path = os.path.join(DATADIR,category)  # create path to dogs and cats
+        class_num = CATEGORIES.index(category)  # get the classification  (0 or a 1). 0=dog 1=cat
+
+        for img in tqdm(os.listdir(path)):  # iterate over each image per dogs and cats
+            try:
+                img_array = cv2.imread(os.path.join(path,img) ,cv2.IMREAD_GRAYSCALE)  # convert to array
+                new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))  # resize to normalize data size
+                training_data.append([new_array, class_num])  # add this to our training_data
+            except Exception as e:  # in the interest in keeping the output clean...
+                pass
+            #except OSError as e:
+            #    print("OSErrroBad img most likely", e, os.path.join(path,img))
+            #except Exception as e:
+            #    print("general exception", e, os.path.join(path,img))
+
+create_training_data()
+
+print(len(training_data))
