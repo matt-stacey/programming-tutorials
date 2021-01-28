1dbe09c72a54709ca96a6c96e3e9e779e2f9fb42
Matt Stacey FX505DU-manjaro <53570754+matt-stacey@users.noreply.github.com>
2021-01-28 15:20:32 -0600

    pyglet windows

--- Python/PygletBasics/windows.py
+++ Python/PygletBasics/windows.py
@@ -1,35 +1,43 @@
 import pyglet
 
 
-window = pyglet.window.Window(1280, 720,
-                              resizable=True,
-                              visible=False,
-                              caption='Window caption',
-                              vsync=False,
-                              screen=pyglet.canvas.get_display().get_screens()[0],
-                              )
+class MyWindow(pyglet.window.Window):
+    def __init__(self):
+        super(MyWindow, self).__init__(1280, 720,
+                                       resizable=True,
+                                       visible=False,
+                                       caption='Window caption',
+                                       vsync=False,
+                                       screen=pyglet.canvas.get_display().get_screens()[0],
+                                       )
 
-@window.event
-def on_resize(width, height):
-    print('The window was resized to {}x{}'.format(width, height))
+        self.set_minimum_size(320, 200)
+        self.set_maximum_size(1440, 800)
+        #self.set_size(1280, 720)
 
-"""
-super annoying...change full-/not full-screen on every mouse click
-@window.event
-def on_mouse_press(x, y, button, modifiers):
-    window.set_fullscreen(not window.fullscreen)
-"""
+        x, y = self.get_location()
+        self.set_location(x - 20, y - 20)
 
-window.set_minimum_size(320, 200)
-window.set_maximum_size(1440, 800)
-window.set_size(1280, 720)
+        self.set_caption('New caption')
+        self.label = pyglet.text.Label('Hello, world!')
 
-x, y = window.get_location()
-window.set_location(x - 20, y - 20)
+        self.set_visible()
 
-window.set_caption('New caption')
+    def on_resize(self, width, height):
+        print('The window was resized to {}x{}'.format(width, height))
+        self.label.draw()
 
-window.set_visible()
+    def on_draw(self):
+        self.clear()
+        self.label.draw()
+
+    """
+    super annoying...change full-/not full-screen on every mouse click
+    def on_mouse_press(x, y, button, modifiers):
+        window.set_fullscreen(not window.fullscreen)
+    """
+
 
 if __name__ == '__main__':
+    window = MyWindow()
     pyglet.app.run()
